97c351e544669f4270f08d2110c9d794f7462f35
xmksz3 <markus.sanderst@gmail.com>
2020-01-07 20:26:41 -0600

    added icons

--- src/containers/Skills.js
+++ src/containers/Skills.js
@@ -14,6 +14,9 @@ export default class Skills extends Component {
                 <h2 className="title">my skills</h2>
                 <div className="columns">
                     <div className="column is-light">
+                        <div className="icon is-large">
+                            <i className="fab fa-node-js"></i>
+                        </div>
                         <h2 className="has-text-info">
                             front end web development
                         </h2>
@@ -22,6 +25,9 @@ export default class Skills extends Component {
                         </p>
                     </div>
                     <div className="column is-dark">
+                        <div className="icon is-large">
+                            <i className="fas fa-database"></i>
+                        </div>
                         <h2 className="has-text-info">
                             back end web development
                         </h2>
@@ -33,6 +39,9 @@ export default class Skills extends Component {
                    <div className="colum is-one-third is-dark">
                        <div className="columns">
                             <div className="column is-half is-light">
+                                <div className="icon is-large">
+                                    <i className="fas fa-house-damage"></i>
+                                </div>
                                 <h2 className="has-text-info">
                                     insurance
                                 </h2>
@@ -42,6 +51,9 @@ export default class Skills extends Component {
                                 </p>
                             </div>
                             <div className="column is-half is-light">
+                                <div className="icon is-large">
+                                    <i className="fas fa-music"></i>
+                                </div>
                                 <h2 className="has-text-info">
                                     music performance
                                 </h2>
