52214e5aa85cc27cb1d9f973c085afac39273866
xmksz3 <markus.sanderst@gmail.com>
2020-01-09 20:39:08 -0600

    adding blog photos and titles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Bio from './components/Bio';
 import Navbar from './components/Navbar';
 import Skills from './containers/Skills';
 import Contact from './containers/Contact';
+import Blogs from './containers/Blogs';
 
 export default class App extends Component {
   constructor(props) {
@@ -22,6 +23,7 @@ export default class App extends Component {
         <Head />
         <Bio />
         <Skills />
+        <Blogs />
         <Contact />
       </div>
     )
---
+++ src/containers/Blogs.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import oprah from '../photos/oprah-giveaway.gif';
+import boatingSchool from '../photos/boating-school.gif';
+import developer from '../photos/developer.gif';
+
+const Blogs = props => {
+
+
+    return (
+        <section className="has-text-centered">
+            <h2 className="title" id="blogs">my blogs</h2>
+            <div className="columns is-multiline">
+                <div className="column blog-column">
+                    <div className="container blog-image-container">
+                        <figure className="image">
+                            <img className="blog-image" src={oprah} alt="gif of Oprah during car giveaway" />
+                        </figure>
+                    </div>
+                    <h2 className="subtitle blog-title">rebuilding JavaScript's built-in higher-order functions</h2>
+                </div>
+                <div className="column blog-column">
+                    <div className="container blog-image-container">
+                        <figure className="image">
+                            <img className="blog-image" src={developer}  alt="gif of developer working at desk"/>
+                        </figure>
+                    </div>
+                    <h2 className="subtitle blog-title">optimizing your coding process</h2>
+                </div>
+                <div className="column blog-column">
+                    <div className="container blog-image-container">
+                        <figure className="image">
+                            <img className="blog-image" src={boatingSchool} alt="gif of Mrs. Puff from Spongebob, 'What I learned in boating school is...'"/>
+                        </figure>
+                    </div>
+                    <h2 className="subtitle blog-title">what i learned in coding school is...</h2>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Blogs;
