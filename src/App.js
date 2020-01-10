4a954d8f58a5e83429d08d3e5e067f9cb42d6b0f
xmksz3 <markus.sanderst@gmail.com>
2020-01-10 15:46:49 -0600

    check in

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
     this.state = {
       prevScrollPos: window.pageYOffset,
       visible: null,
-      navClass: ""
+      navClass: "navbar-1--hidden is-fourth"
     }
   }
 
@@ -43,12 +43,16 @@ export default class App extends Component {
 
   render(){
     return (
-      <div >
+      <div className="is-white">
         <Navbar className={this.state.navClass}/>
         <Head />
+        <br />
         <Bio />
+        <br />
         <Skills />
+        <br />
         <Blogs />
+        <br />
         <Contact />
       </div>
     )
