48c30c42686df4ab35ad3e9f52c82dbc9574f365
xmksz3 <markus.sanderst@gmail.com>
2020-01-10 23:54:20 -0600

    added images, hover effect on hero

--- src/containers/Head.js
+++ src/containers/Head.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
-import Avatar from 'avataaars';
 import Welcome from '../components/Welcome';
+import frown from '../photos/markus-frown-laugh.jpg';
+import avataaar from '../images/avatar-angry-laugh.svg';
 
 export default class Head extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            hover: false,
         };
+    }
+
+    hoverTrue = () => {
+        this.setState({ hover: true });
+    }
+
+    hoverFalse = () => {
+        this.setState({ hover: false });
+    }
+
+    renderImage = () => {
+        if (!this.state.hover) {
+            return <img className="" src={avataaar} alt="avataaar of markus frowning/smiling" onMouseEnter={() => this.hoverTrue()} />
+        } else {
+            return <img className="profile-pic" src={frown} alt="markus frowning/smiling" /> 
+        }
     }
 
     render() {
@@ -14,19 +32,9 @@ export default class Head extends Component {
             <section className="hero is-sixth is-medium">
                 <div className="hero-body">
                     <div className="container has-text-centered">
-                        <Avatar
-                            avatarStyle='Circle'
-                            topType='ShortHairTheCaesar'
-                            accessoriesType='Blank'
-                            hairColor='Black'
-                            facialHairType='Blank'
-                            clotheType='ShirtVNeck'
-                            clotheColor='Gray02'
-                            eyeType='Happy'
-                            eyebrowType='AngryNatural'
-                            mouthType='Smile'
-                            skinColor='Black'
-                        />
+                        <div className="image-crop"  onMouseLeave={() => this.hoverFalse()}>
+                            {this.renderImage()}
+                        </div>                       
                         <Welcome />
                     </div>
                 </div>
